e702c329ff7067782935abb36ba2d66b677bec81
hanggarap <35048788+hanggarap@users.noreply.github.com>
2018-03-08 09:23:21 +1100

    Fix primeNg Support
    Adding shared.content

---
+++ AngularBoilerplate/Angular.Web/src/app/shared/shared.content.ts
@@ -0,0 +1,18 @@
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'primeng/accordion';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+
+export const modules = [
+  FormsModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule,
+  AccordionModule,
+  InputTextModule,
+  ToggleButtonModule,
+]
--- AngularBoilerplate/Angular.Web/src/app/shared/shared.module.ts
+++ AngularBoilerplate/Angular.Web/src/app/shared/shared.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
-import {
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule } from 'primeng/accordion';
-import { InputTextModule } from 'primeng/inputtext';
+import { modules } from './shared.content';
 
 @NgModule({
   imports: [
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AccordionModule,
-    InputTextModule,
+    ...modules
   ],
   providers: [],
+  exports: [
+    ...modules
+  ]
 })
 
 export class SharedModule { }
